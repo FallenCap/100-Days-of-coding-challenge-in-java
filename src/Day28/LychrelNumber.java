22c28ea8f452416834e023449e1d90db221f723f
FallenCap <fallencap18@gmail.com>
2022-11-09 13:18:35 +0530

    Adding comments in some programs

--- src/Day28/LychrelNumber.java
+++ src/Day28/LychrelNumber.java
@@ -1,4 +1,4 @@
-/* Write a java program to print Lychrel number in a given range. 
+/* Write a java program to print all  Lychrel numbers in a given range. 
 A Lychrel number is a natural number that cannot form a palindrome through the iterative process of repeatedly reversing its digits and adding the resulting numbers. This process is sometimes called the 196-algorithm, after the most famous number associated with the process.
 The first few Lychrel numbers are 196, 295, 394, 493, 592, 689, 691, 788, 790, 879, 887, ... .
  */
