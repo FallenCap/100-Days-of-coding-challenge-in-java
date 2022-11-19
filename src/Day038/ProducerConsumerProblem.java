1a892cb89b5b7d0c827cb5d070bfc73f1c8ab472
FallenCap <fallencap18@gmail.com>
2022-11-19 20:25:41 +0530

    Day 38

--- src/Day038/ProducerConsumerProblem.java
+++ src/Day038/ProducerConsumerProblem.java
@@ -1,21 +1,13 @@
 /*Write a java program to implement producer consumer probelem.*/
 
 package Day038;
-
-public class ProducerConsumerProblem {
-      public static void main(String[] args) {
-            Shop c = new Shop();
-            Producer p1 = new Producer(c, 1);
-            Consumer c1 = new Consumer(c, 1);
-            p1.start();
-            c1.start();
-      }
-}
 class Shop {
+	//Creating class for the producer-consumer problem.
       private int materials;
       private boolean available = false;
       
       public synchronized int get() {
+    	  //Method for getting item 
             while (available == false) {
                   try {
                         wait();
@@ -29,8 +21,8 @@ class Shop {
             notifyAll();
             return materials;
       }
-      public synchronized void put(int value)
-      {
+      public synchronized void put(int value) {
+    	  //Method for putting element.
             while (available == true)
             {
                   try
@@ -47,12 +39,15 @@ class Shop {
             notifyAll();
       }
 }
-class Consumer extends Thread
-{
+class Consumer extends Thread {
+	//Class for cosumer problem.
+	
+	//Declaring variables globally.
       private Shop Shop;
       private int number;
-      public Consumer(Shop c, int number)
-      {
+      
+      public Consumer(Shop c, int number) {
+    	  //Mehtod for consumer problem.
             Shop = c;
             this.number = number;
       }
@@ -66,13 +61,15 @@ class Consumer extends Thread
             }
       }
 }
-class Producer extends Thread
-{
+class Producer extends Thread {
+	  //Method for producer problem.
+	
+	//Declaring variables globally.
       private Shop Shop;
       private int number;
 
-      public Producer(Shop c, int number)
-      {
+      public Producer(Shop c, int number) {
+    	//Mehtod for producer problem.
             Shop = c;
             this.number = number;
       }
@@ -93,3 +90,36 @@ class Producer extends Thread
             }
       }
 }
+public class ProducerConsumerProblem {
+    public static void main(String[] args) {
+          Shop c = new Shop();
+          Producer p1 = new Producer(c, 1);
+          Consumer c1 = new Consumer(c, 1);
+          p1.start();
+          c1.start();
+    }
+}
+
+
+//-------------Test Case-------------
+//Output: 
+//Consumed value 1 got: 0
+//Produced value 1 put: 0
+//Produced value 1 put: 1
+//Consumed value 1 got: 1
+//Produced value 1 put: 2
+//Consumed value 1 got: 2
+//Consumed value 1 got: 3
+//Produced value 1 put: 3
+//Produced value 1 put: 4
+//Consumed value 1 got: 4
+//Produced value 1 put: 5
+//Consumed value 1 got: 5
+//Produced value 1 put: 6
+//Consumed value 1 got: 6
+//Consumed value 1 got: 7
+//Produced value 1 put: 7
+//Consumed value 1 got: 8
+//Produced value 1 put: 8
+//Consumed value 1 got: 9
+//Produced value 1 put: 9
