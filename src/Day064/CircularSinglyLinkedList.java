4202f825b62634479253dafcccdb3e81d6f34650
FallenCap <fallencap18@gmail.com>
2023-01-02 00:05:57 +0530

    Day 64

---
+++ src/Day064/CircularSinglyLinkedList.java
@@ -0,0 +1,106 @@
+/*Implement Circular Singly Linked List using  java.*/
+
+package Day064;
+
+//Creating class for Nodes of the  linked list.
+class Node {
+	public int data;
+	public Node next;
+	
+	//Method for display nodes of the linkedlist.
+	public void displayNode() {
+		System.out.println(" { " + data + " } ");
+	}
+}
+
+public class CircularSinglyLinkedList {
+	
+	
+	private Node head;
+	private Node tail;
+	
+	//Method for inserting new node to a singly linked list.
+	public void insert(int  data) {
+		
+		Node newNode = new Node(); //Creating  a  new Node.
+		 newNode.data = data; //Entering data in the  new node.
+		 
+		 if(head == null) {
+			 //if  head is null then the head will point  the new node.
+			 head = newNode;
+			 tail = newNode;
+			 newNode.next = head;
+			 
+		 }
+		 else {
+			 tail.next = newNode;//tail will point to new node.
+			 tail = newNode;//New node will become new tail.
+			 tail.next = head;//now new taill will point head.
+		 }
+	}
+	
+	
+	//Method for display the linked list.
+	public void displayList() {
+		System.out.println("List (first --> last-->first) ");
+		Node current = head; //create current node and assigning as head.
+		do {
+			//running the loop till current not equals to null.
+			current.displayNode(); // Calling the displayNode() method.
+			current = current.next;
+		}while(current != head);
+		
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		
+		CircularSinglyLinkedList list = new CircularSinglyLinkedList();
+		
+		list.insert(10);
+		list.insert(20);
+		list.insert(30);
+		list.insert(40);
+		list.insert(50);
+		list.insert(60);
+		list.insert(70);
+		list.insert(80);
+		list.insert(90);
+		list.insert(100);
+		
+		list.displayList();
+
+	}
+
+}
+
+
+//------------ Test Cases -----------
+
+//TestCase1:
+//Input: 100, 200, 300, 400, 500, 600, 700.
+//Output:
+//List (first --> last-->first) 
+//{ 100 } 
+//{ 200 } 
+//{ 300 } 
+//{ 400 } 
+//{ 500 } 
+//{ 600 } 
+//{ 700 } 
+
+
+//TestCase2:
+//Input: 10,20, 30, 40, 50, 60, 70, 80, 90, 100
+//Output:
+//List (first --> last-->first) 
+//{ 10 } 
+//{ 20 } 
+//{ 30 } 
+//{ 40 } 
+//{ 50 } 
+//{ 60 } 
+//{ 70 } 
+//{ 80 } 
+//{ 90 } 
+//{ 100 } 
