9bdb44fdfbf14334cf2f633bd463374bbafb96bd
FallenCap <fallencap18@gmail.com>
2023-01-26 18:54:45 +0530

    Day 88

--- src/Day088/BubbleSort.java
+++ src/Day088/BubbleSort.java
@@ -11,7 +11,10 @@ public class BubbleSort {
 	        for (int i = array.length - 1; i > 0; i--) {
 	        	//the loop will iterate through from the last to first element of the array.
 	            for (int j = 0; j < i; j++) {
+	            	//This loop will run opposite of the first loop.
 	                if (array[j] > array[j+1]) {
+	                	/*If the array element is lareger than then its next array element
+	                	Then it will swap its position by using a third variable.*/
 	                    int temp = array[j];
 	                    array[j] = array[j+1];
 	                    array[j+1] = temp;
@@ -19,25 +22,24 @@ public class BubbleSort {
 	            }
 	        }
 	    }
+	 
+	 public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the length of the array: ");
+			int n = sc.nextInt();
+			
+			int[] myArray = new int[n];
+			
+			System.out.println("Enter the array elements: ");
+			for(int i = 0; i < n; i++) {
+				myArray[i] = sc.nextInt();
+			}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the length of the array: ");
-		int n = sc.nextInt();
-		
-		int[] myArray = new int[n];
-		
-		System.out.println("Enter the array elements: ");
-		for(int i = 0; i < n; i++) {
-			myArray[i] = sc.nextInt();
-		}
+	        bubbleSort(myArray);
 
-        bubbleSort(myArray);
-
-        System.out.println("Array after Bubble Sort: " + Arrays.toString(myArray) );
-        sc.close();
-	}
-
+	        System.out.println("Array after Bubble Sort: " + Arrays.toString(myArray) );
+	        sc.close();
+	 }
 }
 
 
@@ -57,4 +59,19 @@ Array after Bubble Sort: [2, 13, 21, 26, 32]
 
 
 //TestCase2:
-/**/
+/*
+ Enter the length of the array: 
+10
+Enter the array elements: 
+10
+21
+24
+32
+56
+43
+78
+2
+1
+76
+Array after Bubble Sort: [1, 2, 10, 21, 24, 32, 43, 56, 76, 78]
+*/
