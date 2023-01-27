794c69a276f32b8e508bfe0d94c039e10043fd4e
FallenCap <fallencap18@gmail.com>
2023-01-27 10:23:36 +0530

    Day 90

---
+++ src/Day090/MergeSort.java
@@ -0,0 +1,55 @@
+package Day090;
+
+import java.util.*;
+
+public class MergeSort {
+
+	//Method for merge sort.
+	public static int[] merge(int[] array1, int[] array2) {
+        int[] combined = new int[array1.length + array2.length];
+        int index = 0;
+        int i = 0;
+        int j = 0;
+        while (i < array1.length && j < array2.length) {
+            if (array1[i] < array2[j]) {
+                combined[index] = array1[i];
+                index++;
+                i++;
+            } else {
+                combined[index] = array2[j];
+                index++;
+                j++;
+            }
+        }
+        while (i < array1.length) {
+            combined[index] = array1[i];
+            index++;
+            i++;
+        }
+        while (j < array2.length) {
+            combined[index] = array2[j];
+            index++;
+            j++;
+        }
+        return combined;
+    }
+	
+	public static void main(String[] args) {
+
+		int[] array1 = {10, 32, 3, 43,12, 87};
+        int[] array2 = {2,4,5,6};
+
+        System.out.println("Array after merge sort: " +  Arrays.toString( merge(array1, array2) ) );
+	}
+
+}
+
+
+//------------ Test Cases -----------
+
+//TestCase1:
+//Array after merge sort: [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+//TestCase2: 
+//
