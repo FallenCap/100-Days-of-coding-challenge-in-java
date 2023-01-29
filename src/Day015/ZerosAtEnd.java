45cfeb3f156f1a738d0362a68d432d75fc6fe647
FallenCap <fallencap18@gmail.com>
2023-01-29 16:45:28 +0530

    Day 15

--- src/Day015/ZerosAtEnd.java
+++ src/Day015/ZerosAtEnd.java
@@ -1,4 +1,4 @@
-//Write a Java program to move all 0's to the end of an array. Maintain the relative order of the other (non-zero) array elements.
+/*Write a Java program to move all 0's to the end of an array. Maintain the relative order of the other (non-zero) array elements.*/
 
 package Day015;
 
