f13c5e37db6702a868b79aae10e26a25d0703df9
FallenCap <fallencap18@gmail.com>
2023-01-29 16:45:22 +0530

    Day 14

--- src/Day014/OvertimePay.java
+++ src/Day014/OvertimePay.java
@@ -1,6 +1,6 @@
-//Write a program to calculate overtime pay of a number employees which is inputed by user. Overtime is paid at the rate of
-//Rs. 12.00 per hour for every hour worked above 40 hours. Assume that employees do not
-//work for fractional part of an hour.
+/*Write a program to calculate overtime pay of a number employees which is inputed by user. Overtime is paid at the rate of
+Rs. 12.00 per hour for every hour worked above 40 hours. Assume that employees do not
+work for fractional part of an hour.*/
 
 package Day014;
 
