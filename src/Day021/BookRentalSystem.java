5c7df9fbbd9d29d9f0d44e8421982e8dac895997
FallenCap <fallencap18@gmail.com>
2023-01-29 16:46:28 +0530

    Day 21

--- src/Day021/BookRentalSystem.java
+++ src/Day021/BookRentalSystem.java
@@ -1,4 +1,4 @@
-//The libraries of SmallTownX need a new electronic rental system, and it is up to you to build it. SmallTownX has two libraries. Each library offers many books to rent. Customers can print the list of available books, borrow, and return books.
+/*The libraries of SmallTownX need a new electronic rental system, and it is up to you to build it. SmallTownX has two libraries. Each library offers many books to rent. Customers can print the list of available books, borrow, and return books.*/
 //Define two classes, Book and Library, that provide the functionality for the book database.
 
 package Day021;
